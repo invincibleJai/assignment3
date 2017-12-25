7d6ecb9e1b8af1ee23565dcf7048658be8481305
invinciblejai <mailjai.vardhan@gmail.com>
2017-12-25 19:09:55 +0530

    initial commit
    Signed-off-by: invinciblejai <mailjai.vardhan@gmail.com>

---
+++ src/app/toggle/toggle.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle',
+  templateUrl: './toggle.component.html',
+  styleUrls: ['./toggle.component.css']
+})
+export class ToggleComponent implements OnInit {
+
+  displayPg:boolean = true;
+  counters:any = [];
+  count:number = 0;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  triggerBtnClick(){
+    this.displayPg = !this.displayPg;
+    this.counters.push(this.count++);
+    console.log('yay!!');
+  }
+
+  getBgColor(counter){
+    return counter > 4 ? 'blue' : 'none';
+  }
+
+  getTxtColor(counter){
+    return counter > 4 ? true : false;
+  }
+}
